840dfa2d3e504338eb89fcbf2cb8ed9fb4650ada
pramod <pramod@PRAMODHP14>
2021-09-11 13:13:40 +0530

    commit

---
+++ NoOfWeek/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDemo
+{
+
+    class Program
+    {
+        enum Days { Mon, Tue, Wed, Thu, Fri, Sat, Sun };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enum Demo");
+            int firstday = (int)Days.Mon;
+            int lastday = (int)Days.Sun;
+            Console.WriteLine("Mon :{0}", firstday);
+            Console.WriteLine("Sun:{0}", lastday);
+            Console.ReadLine();
+           
+
+        }
+    }
+}
